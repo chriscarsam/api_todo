6662b3c583f17a221f6a5af3ff907f1fd3488908
Christian <chriscar.sam@gmail.com>
2023-06-06 11:35:43 -0500

    Entidad Task

---
+++ src/main/java/com/sam/todoapp/persistence/entity/Task.java
@@ -0,0 +1,20 @@
+package com.sam.todoapp.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Strint title;
+    private String description;
+    private LocalDateTime createDate;
+    private LocalDateTime eta;
+    private boolean finished;
+    private TaskStatus taskStatus;
+}
---
+++ src/main/java/com/sam/todoapp/persistence/entity/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.sam.todoapp.persistence.entity;
+
+public enum TaskStatus {
+    ON_TIME, LATE
+}
