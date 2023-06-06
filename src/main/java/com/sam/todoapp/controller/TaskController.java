37b47d5b272236fd67b954a6580d0c3a445e4bcb
Christian <chriscar.sam@gmail.com>
2023-06-06 12:13:45 -0500

    POST crear task

--- src/main/java/com/sam/todoapp/controller/TaskController.java
+++ src/main/java/com/sam/todoapp/controller/TaskController.java
@@ -1,7 +1,22 @@
 package com.sam.todoapp.controller;
 
+import com.sam.todoapp.persistence.entity.Task;
+import com.sam.todoapp.service.TaskService;
+import com.sam.todoapp.service.dto.TaskInDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+    }
 }
--- src/main/java/com/sam/todoapp/service/TaskService.java
+++ src/main/java/com/sam/todoapp/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.sam.todoapp.service;
 
+import com.sam.todoapp.mapper.TaskInDTOToTask;
 import com.sam.todoapp.persistence.entity.Task;
 import com.sam.todoapp.persistence.repository.TaskRepository;
 import com.sam.todoapp.service.dto.TaskInDTO;
@@ -9,12 +10,15 @@ import org.springframework.stereotype.Service;
 public class TaskService {
 
     private final TaskRepository repository;
+    private final TaskInDTOToTask mapper;
 
-    public TaskService(TaskRepository repository) {
+    public TaskService(TaskRepository repository, TaskInDTOToTask mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     public Task createTask(TaskInDTO taskInDTO){
-        this.repository.save()
+        Task task = mapper.map(taskInDTO);
+        return this.repository.save(task);
     }
 }
